cfe35deb1b8178640ce63e0aa93abed6770f6532
sambarilove <50455059+sambarilove@users.noreply.github.com>
2019-12-09 22:43:36 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const http = require('http')
+
+const requisicao = function(request, response){
+    response.writeHead(200,{'Content-Type':'text/html'})
+    response.write('<h1>Texto teste</h1>')
+    response.end()
+
+}
+
+const server = http.createServer(requisicao)
+
+const resultado = function(){
+    console.log('Servidor em funcionamento')    
+}
+
+server.listen(3000, resultado)
